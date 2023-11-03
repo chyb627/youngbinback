a801e20efc93a0abe66c2436bfeeaeaa8e089e07
cha <chyb627@naver.com>
2023-11-03 11:31:06 +0900

    /subs/sub/topSubs (게시물 보기) api

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -6,6 +6,7 @@ import authMiddleware from '@/middlewares/auth';
 import { isEmpty } from 'class-validator';
 import { AppDataSource } from '@/data-source';
 import Sub from '@/entities/Sub';
+import Post from '@/entities/Post';
 
 const createSub = async (req: Request, res: Response, next: NextFunction) => {
   const { name, title, description } = req.body;
@@ -46,8 +47,28 @@ const createSub = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
+const topSubs = async (_: Request, res: Response) => {
+  try {
+    const imageUrlExp = `COALESCE(s."imageUrn", 'https://www.gravatar.com/avatar?d=mp&f=y')`;
+    const subs = await AppDataSource.createQueryBuilder()
+      .select(`s.title, s.name, ${imageUrlExp} as "imageUrl", count(p.id) as "postCount"`)
+      .from(Sub, 's')
+      .leftJoin(Post, 'p', `s.name = p."subName"`)
+      .groupBy('s.title, s.name, "imageUrl"')
+      .orderBy(`"postCount"`, 'DESC')
+      .limit(5)
+      .execute();
+    console.log('subs::', subs);
+    return res.json(subs);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
 const router = Router();
 
 router.post('/', userMiddleware, authMiddleware, createSub);
+router.get('/sub/topSubs', topSubs);
 
 export default router;
